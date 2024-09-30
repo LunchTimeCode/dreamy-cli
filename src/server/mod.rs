7574d9cdf836030e9903a77684739d549d8baac4
Silen Locatelli <88315530+SilenLoc@users.noreply.github.com>
2024-09-30 16:06:49 +0200

    intermediate state

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,14 +1,16 @@
 use appstate::AppState;
+
 use axum::{
-    extract::State,
+    extract::{self, State},
     http::StatusCode,
     response::{IntoResponse, Response},
-    routing::get,
+    routing::{get, post},
     Router,
 };
 use colored::Colorize;
 use poll_schedule::PollSchedule;
-use std::sync::Arc;
+use serde_derive::Deserialize;
+use std::{borrow::BorrowMut, sync::Arc};
 use tokio::net::TcpListener;
 use tokio::spawn;
 use tokio::sync::Mutex;
@@ -129,6 +131,7 @@ fn make_api() -> Router<Arc<Mutex<AppState>>> {
     Router::new()
         .route("/healthz", get(healthz))
         .route("/", get(view))
+        .route("/repos", post(set_repos))
 }
 
 async fn healthz() -> Response {
@@ -138,4 +141,18 @@ async fn healthz() -> Response {
 async fn view(State(app_state): State<Arc<Mutex<AppState>>>) -> axum::response::Html<String> {
     let state = app_state.lock().await;
     axum::response::Html(state.get_html().to_string())
+}
+
+#[derive(Deserialize, Debug)]
+struct Repos {
+    repos: Vec<String>,
+}
+
+async fn set_repos(
+    State(app_state): State<Arc<Mutex<AppState>>>,
+    extract::Json(payload): extract::Json<Repos>,
+) -> Response {
+    let mut state = app_state.lock().await.borrow_mut();
+    state.replace_repos(payload.repos);
+    StatusCode::OK.into_response()
 }
